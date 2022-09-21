c4f54852c6a4df347eb6aff8dd0acd82f738938a
Abdul Aziz <abdulazizsapra@outlook.com>
2022-09-21 08:23:55 +0500

    added code comments

--- index.php
+++ index.php
@@ -1,12 +1,35 @@
 <?php
 
+/**
+ * name: php-memory-profiler
+ * author: abdulazizsapra
+ * terminal cmds used: top, tasklist  
+ * global variables: PHP_OS
+ */
+
+/** Linux Compatibility | Start
+ */
+
 if (PHP_OS == 'Linux') {
     echo "<center>";
+    //  Onload this cmd runs and shows all the processes by default.
     $cmd = shell_exec('/usr/bin/top -b -n1');
+
+
+    /** Search Functionality | Start
+     */
+
     if (isset($_GET['pid']) && $_GET['pid']) {
         $pid = $_GET['pid'];
         $cmd = shell_exec('/usr/bin/top -p ' . $pid . ' -b -n1');
     }
+
+    /** Search Functionality | End
+     */
+
+
+    /** Format the cmd output | Start
+     */
     $cmd = str_replace("      ", " ", $cmd);
     $cmd = str_replace("     ", " ", $cmd);
     $cmd = str_replace("    ", " ", $cmd);
@@ -16,15 +39,24 @@ if (PHP_OS == 'Linux') {
     $cmd = str_replace("\n", "</td></tr><tr><td>", $cmd);
     $cmd = str_replace("<tr><td></td><td>", "<tr><td>", $cmd);
     $cmd = str_replace("<tr><td>PID", "<tr><td COLSPAN=10 height=50></td></tr><tr bgcolor=e0e0e0><td>PID", $cmd);
+    /** Format the cmd output | End
+     */
 
+    /** Render Display | Start
+     */
     echo "<form>Please Enter Process ID to see details: <input type='number' name='pid' /> <input type='submit'></form>";
     echo '<table width=900 align=middle border=0><tr><td>';
     echo $cmd;
     echo '</td></tr></table>';
+    /** Render Display | End
+     */
 }
+/** Linux Compatibility | End
+ */
 
+/** Windows Compatibility | Start
+ */
 if (PHP_OS == 'WINNT') {
-    // echo PHP_OS;
     $output = null;
     $resultCode = null;
     $cmd = "tasklist";
@@ -45,3 +77,5 @@ if (PHP_OS == 'WINNT') {
     }
     echo "</pre>";
 }
+/** Windows Compatibility | End
+ */
